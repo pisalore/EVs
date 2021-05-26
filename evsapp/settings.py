6d27114436009096336ba68588312275427ed3cd
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-05-26 10:01:02 +0200

    Heroku setup

--- evsapp/settings.py
+++ evsapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_EVS')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'evs.herokuapp.com']
 
 # Login
 LOGIN_URL = "/accounts/login/"
