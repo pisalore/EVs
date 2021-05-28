d929ecb75099e09d9952d13186a0908e4e18a180
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-05-28 18:08:32 +0200

    user profile image field related to a aws document object

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, User
 from django.db import models
 
+from aws.models import AWSDocument
+
 
 class EvUser(AbstractUser):
     organization_name = models.CharField(max_length=240)
@@ -9,6 +11,7 @@ class EvUser(AbstractUser):
     is_organizer = models.BooleanField(default=False)
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
+    profile_image = models.OneToOneField(AWSDocument, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.username
