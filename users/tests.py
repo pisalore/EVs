87b8f0fbede6c450692e31075d3c5215f7269791
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-05-29 16:24:27 +0200

    Test file upload

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,10 @@
 import json
-
+from PIL import Image
+from io import BytesIO
 from users.models import EvUser
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -61,12 +64,14 @@ class RetrieveUserInfoTest(APITestCase):
         self.user = EvUser.objects.create_user(username="test",
                                                password='test_password123',
                                                email="test@mail.it",
-                                               is_organizer=False)
+                                               is_organizer=False,
+                                               profile_image=None)
         self.organizer = EvUser.objects.create_user(username="test_org",
                                                     organization_name="org",
                                                     password='test_password123',
                                                     email="test_org@mail.it",
-                                                    is_organizer=True)
+                                                    is_organizer=True,
+                                                    profile_image=None)
 
     def test_retrieve_user_main_info(self):
         self.client.force_authenticate(user=self.user)
@@ -77,7 +82,8 @@ class RetrieveUserInfoTest(APITestCase):
             json.loads(response.content),
             {"username": "test",
              "organization_name": "",
-             "is_organizer": False})
+             "is_organizer": False,
+             "profile_image": None})
 
     def test_retrieve_organizer_main_info(self):
         self.client.force_authenticate(user=self.organizer)
@@ -88,4 +94,33 @@ class RetrieveUserInfoTest(APITestCase):
             json.loads(response.content),
             {"username": "test_org",
              "organization_name": "org",
-             "is_organizer": True})
+             "is_organizer": True,
+             "profile_image": None, })
+
+
+class UploadFileTest(APITestCase):
+    def setUp(self):
+        self.user = EvUser.objects.create_user(username="test",
+                                               password='test_password123',
+                                               email="test@mail.it",
+                                               is_organizer=False,
+                                               profile_image=None)
+        self.url = "/api/user/profile-image/"
+
+    def test_image_upload(self):
+        self.client.force_authenticate(user=self.user)
+
+        stream = BytesIO()
+        image = Image.new('RGB', (100, 100))
+        image.save(stream, format='jpeg')
+
+        uploaded_file = SimpleUploadedFile("file.jpg", stream.getvalue(), content_type="image/jpg")
+        data = {
+            "profile_image.type": "PI",
+            "profile_image.document": uploaded_file,
+            "profile_image.loaded_by": self.user.id
+        }
+
+        response = self.client.put(self.url, data, format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
