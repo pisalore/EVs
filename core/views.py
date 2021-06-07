40c5e3cae0e5cc19138efd6902664949db4933ba
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-06-07 11:43:09 +0200

    tox tests fix (static files problem)

--- core/views.py
+++ core/views.py
@@ -1,11 +1,12 @@
 from django.views.generic.base import TemplateView
-from django.conf import settings
+import os
 
 
 class IndexTemplateView(TemplateView):
 
     def get_template_names(self):
-        if settings.DEBUG:
+        print(os.getenv('DEBUG'))
+        if os.getenv('DEBUG'):
             template_name = 'index-dev.html'
         else:
             template_name = 'index.html'
