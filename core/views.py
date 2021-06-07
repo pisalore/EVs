d89e9cc873ffc13eaba9f4e199f5e3abaf98e9d8
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-06-07 15:54:17 +0200

    Fixed prod build (DEBUG=False)

--- core/views.py
+++ core/views.py
@@ -5,8 +5,7 @@ import os
 class IndexTemplateView(TemplateView):
 
     def get_template_names(self):
-        print(os.getenv('DEBUG'))
-        if os.getenv('DEBUG'):
+        if os.getenv('DEBUG') == 'True':
             template_name = 'index-dev.html'
         else:
             template_name = 'index.html'
