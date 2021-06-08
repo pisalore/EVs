56776c050407d7db5fa63c5371983f9fa2296df4
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-06-08 11:03:54 +0200

    Added logo to header and footer; footer responsive reworked

--- evsapp/settings.py
+++ evsapp/settings.py
@@ -193,10 +193,9 @@ else:
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
-                    # os.path.join(BASE_DIR, 'assets'),
                     os.path.join(BASE_DIR, 'frontend/dist'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
