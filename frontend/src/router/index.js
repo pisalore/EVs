1c98cbcea76bb4194668e9b8c11989806d500503
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-06-08 14:31:21 +0200

    Form error handling

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About";
+import Events from "../views/Events";
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/events",
+    name: "Events",
+    component: Events,
   },
 ];
 
