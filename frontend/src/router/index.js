1197aeefac9a82b06ba41f6318ae0b02fe05ef57
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-06-10 16:44:52 +0200

    Very first contact us page design

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 import About from "../views/About";
+import ContactUs from "../views/ContactUs";
+import FAQ from "../views/FAQ";
 import Events from "../views/Events";
 import EventDetail from "../views/EventDetail";
 
@@ -17,6 +19,16 @@ const routes = [
     component: About,
   },
   {
+    path: "/faq",
+    name: "FAQ",
+    component: FAQ,
+  },
+  {
+    path: "/contacts",
+    name: "ContactUs",
+    component: ContactUs,
+  },
+  {
     path: "/events",
     name: "Events",
     component: Events,
@@ -24,8 +36,8 @@ const routes = [
   {
     path: "/events/:id",
     component: EventDetail,
-    propd: true,
-  }
+    props: true,
+  },
 ];
 
 const router = createRouter({
