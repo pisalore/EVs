32c2e0fc2e0d5d6841650c7d842a280b224d52e6
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2021-06-12 13:56:54 +0200

    Add category to search object and display them

--- frontend/src/components/events/FilterEvents.vue
+++ frontend/src/components/events/FilterEvents.vue
@@ -8,23 +8,56 @@
           </div>
           <div class="col-xl-3 my-3">
             <div class="input-group-lg">
-              <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter a city...">
+              <input
+                type="text"
+                class="form-control"
+                id="inlineFormInputGroup"
+                placeholder="Enter a city..."
+              />
             </div>
-          </div>
-          <div class="input-group-lg col-xl-3 my-3">
-            <select id="inputState" class="form-control">
-              <option selected>Categories...</option>
-              <option>...</option>
-            </select>
           </div>
           <div class="col-xl-3 my-3">
             <div class="input-group-lg">
-              <input class="form-control" type="date" id="example-date-input1">
+              <input
+                class="form-control"
+                type="date"
+                id="example-date-input1"
+              />
             </div>
             <div class="input-group-lg my-3">
-              <input class="form-control" type="date" id="example-date-input2">
+              <input
+                class="form-control"
+                type="date"
+                id="example-date-input2"
+              />
             </div>
           </div>
+          <div class="dropdown col-xl-3 my-3">
+            <button
+              type="button"
+              class="btn btn-lg btn-outline-primary dropdown-toggle"
+              data-toggle="dropdown"
+            >
+              Select some event category...
+            </button>
+            <div class="dropdown-menu">
+              <a
+                class="dropdown-item"
+                v-for="category in categories"
+                :key="category.id"
+                @click="addCategory(category.category)"
+              >
+                {{ category.category }}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <base-badge
+            v-for="category in searchCategories"
+            :key="category.id"
+            :category="category"
+          ></base-badge>
         </div>
       </div>
     </form>
@@ -32,8 +65,29 @@
 </template>
 
 <script>
+import { apiService } from "../../common/api.service";
+import BaseBadge from "../../ui/BaseBadge";
+
 export default {
   name: "FilterEvents",
+  components: { BaseBadge },
+  data() {
+    return {
+      categories: [],
+      searchCategories: [],
+    };
+  },
+  methods: {
+    addCategory(category) {
+      if (this.searchCategories.indexOf(category) === -1)
+        this.searchCategories.push(category);
+      console.log(this.searchCategories);
+    },
+  },
+  async created() {
+    let endpoint = "api/categories/";
+    this.categories = await apiService(endpoint);
+  },
 };
 </script>
 
--- frontend/src/ui/BaseBadge.vue
+++ frontend/src/ui/BaseBadge.vue
@@ -1,20 +1,22 @@
 <template>
-  <span class="badge" :class="type">
-    {{ text }}
+  <span class="badge mt-5" :class="category">
+    {{ category }}
   </span>
 </template>
 
 <script>
 export default {
-  props: ['type', 'title'],
-  computed: {
-    text() {
-      return this.title.toUpperCase();
-    },
-  },
+  props: ["category"],
 };
 </script>
 
 <style scoped>
-
+.badge {
+  background-color: #ccc;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin-right: 0.5rem;
+}
 </style>
