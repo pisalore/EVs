ea7f9550c355896d87dcc4afa2d4d7589963827c
Lorenzo Pisaneschi <lorenzo.pisaneschi@gmail.com>
2021-06-14 14:30:54 +0200

    very first detail event page layout

--- frontend/src/views/EventDetail.vue
+++ frontend/src/views/EventDetail.vue
@@ -1,21 +1,49 @@
 <template>
-  <div v-if="selectedEvent" class="container">
-    <h1>{{ selectedEvent.name }}</h1>
-    <GoogleMap
-      :api-key="googleApiKey"
-      style="width: 100%; height: 500px"
-      :center="coordinates"
-      :zoom="15"
-    >
-      <Marker :options="{ position: coordinates }" />
-    </GoogleMap>
+  <div v-if="selectedEvent" class="container-fluid">
+    <div class="col-xl-12">
+      <div class="row">
+        <div class="col-xl-6 p-0 m-0">
+          <h1>{{ selectedEvent.name }}</h1>
+          <h2>{{ selectedEvent.venue }}</h2>
+        </div>
+        <div
+          v-if="selectedEvent.event_image"
+          class="col-xl-6 p-0 m-0"
+          style="width: 100%; display: block"
+        >
+          <img
+            :src="selectedEvent.event_image.document"
+            alt=""
+            :class="{
+              'mobile-img-height': isMobile,
+              'desktop-img-height': !isMobile,
+            }"
+          />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xl-6 p-0 m-0">
+          <h1>Other info here...</h1>
+        </div>
+        <div class="col-xl-6 p-0 m-0">
+          <GoogleMap
+            :api-key="googleApiKey"
+            style="width: 100%; height: 500px"
+            :center="coordinates"
+            :zoom="15"
+          >
+            <Marker :options="{ position: coordinates }" />
+          </GoogleMap>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
 import { GoogleMap, Marker } from "vue3-google-map";
-export default defineComponent({
+
+export default {
   components: { GoogleMap, Marker },
   props: ["id"],
   data() {
@@ -31,12 +59,20 @@ export default defineComponent({
     selectedEvent() {
       return this.$store.getters["events/getDetailEvent"];
     },
+    isMobile() {
+      return (
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent
+        ) || window.screen.width < 760
+      );
+    },
   },
   methods: {
     async loadSelectedEvent() {
       const googleGeocodingAPI =
         "https://maps.googleapis.com/maps/api/geocode/json?address=";
       await this.$store.dispatch("events/loadSelectedEvent", this.id);
+      console.log(this.selectedEvent);
       this.eventVenue = this.selectedEvent.venue;
       const endpoint = `${googleGeocodingAPI + this.eventVenue}&key=${
         this.googleApiKey
@@ -49,11 +85,19 @@ export default defineComponent({
   created() {
     this.loadSelectedEvent();
   },
-  // setup() {
-  //   const center = { lat: 40.689247, lng: -74.044502 };
-  //   return { center };
-  // },
-});
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+img {
+  width: 100%;
+}
+
+.mobile-img-height {
+  height: 40vw;
+}
+
+.desktop-img-height {
+  height: 15vw;
+}
+</style>
