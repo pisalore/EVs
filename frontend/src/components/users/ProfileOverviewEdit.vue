fc1cb25242b7a25c1ac8d9c5e6298506e830210c
Lorenzo Pisaneschi <lorenzo.pisaneschi@gmail.com>
2021-06-20 11:55:27 +0200

    Profile overview edit template completed

--- frontend/src/components/users/ProfileOverviewEdit.vue
+++ frontend/src/components/users/ProfileOverviewEdit.vue
@@ -1,11 +1,39 @@
 <template>
-  <p>hello edit !</p>
+  <div class="mt-5 col-xl-12">
+    <h1 class="title">Edit your account info</h1>
+    Your city will make us able to suggest events near you.
+    <hr />
+    <user-edit-form></user-edit-form>
+  </div>
+  <div class="mt-5 col-xl-12">
+    <hr />
+    <h1 class="title">Delete your account</h1>
+    <p>This action is not reversible. You will lost all your saved EVs.</p>
+    <div class="d-flex justify-content-center">
+      <button type="button" class="btn btn-lg btn-danger">
+        Delete Account
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import UserEditForm from "./UserEditForm";
 export default {
   name: "ProfileOverviewEdit",
+  components: { UserEditForm },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.title {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 28px;
+  color: #1f6dad;
+}
+p {
+  font-weight: bold;
+}
+</style>
---
+++ frontend/src/components/users/UserEditForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" placeholder="Name" />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="surname">Surname</label>
+        <input
+          type="text"
+          class="form-control"
+          id="surname"
+          placeholder="Surname"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group col-md-6">
+        <label for="birthday">Birthday</label>
+        <input type="date" class="form-control" id="birthday" />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="city">City</label>
+        <input type="text" class="form-control" id="city" placeholder="City" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" class="form-control" id="email" placeholder="Email" />
+    </div>
+    <div class="d-flex justify-content-center mt-4">
+      <button type="submit" class="btn btn-lg btn-primary">
+        Update Profile
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "UserEditForm",
+  props: ["user"],
+};
+</script>
+
+<style scoped></style>
