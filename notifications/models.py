ca6ff1091a01641ba937bb10df4f65eb945332c6
Lorenzo Pisaneschi <lorenzo.pisaneschi@gmail.com>
2021-06-21 22:31:20 +0200

    added notification migration

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from events.models import Event
+from users.models import EvUser
 
-# Create your models here.
+
+class Notification(models.Model):
+    type = models.CharField(max_length=20)
+    message = models.CharField(max_length=128)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    members = models.ManyToManyField(EvUser, through='NotificationItem')
+
+    def __str__(self):
+        return self.type + " notification"
+
+
+class NotificationItem(models.Model):
+    user = models.ForeignKey(EvUser, on_delete=models.CASCADE)
+    notification = models.ForeignKey(Notification, on_delete=models.CASCADE)
+    user_has_read = models.BooleanField(default=False)
