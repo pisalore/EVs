7ac4cba75af40f6f1848fb29526f104a35f71c4a
Lorenzo Pisaneschi <lorenzo.pisaneschi@gmail.com>
2021-06-22 11:50:36 +0200

    set read all notifications which have not been read before from an user with a post call

--- notifications/api/views.py
+++ notifications/api/views.py
@@ -1,9 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
 
 from .serializers import NotificationSerializer
-from notifications.models import Notification
+from notifications.models import Notification, NotificationItem
 
 
 class UserNotificationsListAPIView(generics.ListAPIView):
@@ -12,4 +13,18 @@ class UserNotificationsListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        return Notification.objects.order_by('created_at').filter(notified__username=user.username)
+        return Notification.objects.order_by('-created_at').filter(notified__username=user.username)
+
+
+class UserNotificationsReadView(APIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user_notification_items = NotificationItem.objects \
+            .filter(user=request.user) \
+            .filter(user_has_read=False)
+        for notification in user_notification_items:
+            notification.user_has_read = True
+            notification.save()
+        return Response(status=status.HTTP_200_OK)
